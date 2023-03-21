eab9189da76ec515de93d45dd676aa4b6425bc13
tammy vu <tammy.vsk@gmail.com>
2023-03-21 11:19:51 -0500

    counter

--- TimeDuration.java
+++ TimeDuration.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class TimeDuration {
 
     private int h;
     private int m;
     private int s;
+    public static int counter = 0;
+
 
 
     public TimeDuration(int h, int m, int s){
         this.h = h;
         this.m = m;
         this.s = s;
+        counter ++;
     }
 
     public String toString(){
@@ -33,16 +36,18 @@ public class TimeDuration {
         return result;
     }
 
-    public static parseFromString(String time){
-        System.out.println("ENTER TIME DURATION: ");
+    // public static parseFromString(String time){
+    //     System.out.println("ENTER TIME DURATION: ");
 
-        Scanner scanner = new Scanner(System.in);
-        time = scanner.nextLine();
-    }
+    //     Scanner scanner = new Scanner(System.in);
+    //     time = scanner.nextLine();
+    // }
 
     public static void main(String[] args) {
         TimeDuration time = new TimeDuration(1, 2, 3);
-        System.out.println(time);
+        TimeDuration time2 = new TimeDuration(9, 2, 3);
+        System.out.println(time + "\n" + time2);
+        System.out.println(counter);
         
         
     }
